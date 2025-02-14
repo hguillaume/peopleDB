88fbd5834e285ef485d65b239de54f369c33cb8e
Guillaume Hamilton <h_guillaume@hotmail.com>
2025-02-14 14:13:17 -0500

    Add DB and migrations

---
+++ peopleDB.Server/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using peopleDB.Server.Data;
+
+namespace peopleDB.Server.Controllers
+{
+    // localhost:port/api/Users
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public UsersController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var allUsers = dbContext.Users.ToList();
+            return Ok(allUsers);
+        }
+    }
+}
