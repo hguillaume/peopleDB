9b177e2d68d8af4df489b5d2461b6dd4dd79bada
Guillaume Hamilton <h_guillaume@hotmail.com>
2025-02-19 13:25:46 -0500

    Add Test Units for User Controller

--- peopleDB.Server/Models/Entities/User.cs
+++ peopleDB.Server/Models/Entities/User.cs
@@ -1,10 +1,21 @@
-﻿namespace peopleDB.Server.Models.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace peopleDB.Server.Models.Entities
 {
     public class User
     {
-        public int id { get; set; }
-        public required string name { get; set; }
-        public required string email { get; set; }
-        public required string password { get; set; }
+        public int Id { get; set; }
+        
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(50, ErrorMessage = "Name cannot be longer than 50 characters.")]
+        public required string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address.")]
+        public required string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters long.")]
+        public required string Password { get; set; }
     }
 }
